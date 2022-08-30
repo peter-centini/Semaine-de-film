dc380318e4cb7bbe71660eaedb0e74e4cdcc18c4
Peter <peter@MacBook-Air-de-Peter.local>
2022-08-30 20:43:55 +0200

    first commit apli semaine de film

---
+++ index.js
@@ -0,0 +1,36 @@
+const calendar = new VanillaCalendar({
+    selector: "#myCalendar",
+    onSelect: (data, elem) => {
+        selectedDate = new Date(data.date).toISOString().split('T')[0];
+        // alert(`Vous avez selectionnéle ${selectedDate}`)
+    },
+     months: [
+        "Janvier",
+        "Fevrier",
+        "Mars",
+        "Avril",
+        "Mai",
+        "Juin",
+        "Juillet",
+        "Aout",
+        "Septembre",
+        "Octobre",
+        "Novembre",
+        "Decembre",
+    ],
+    shortWeekday: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
+
+});
+let selectedDate = {};
+const movieForm = document.querySelector('#movie-form');
+movieForm.addEventListener('submit', addMovie);
+
+function addMovie(e) {
+    e.preventDefault();
+    const formData = new FormData(movieForm);
+    const title = formData.get('title');
+    const year = formData.get('year');
+    const duration = formData.get('duration');
+    const genre = formData.getAll('genre');
+    console.log('movie',{title,year,duration,genre,selectedDate});
+}
