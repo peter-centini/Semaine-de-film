e8dc7c4d12eb1fc10b5ff2ab051ddb9747e6ff0b
Peter <peter@MacBook-Air-de-Peter.local>
2022-08-30 20:55:05 +0200

    locale storage

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const calendar = new VanillaCalendar({
     onSelect: (data, elem) => {
         selectedDate = new Date(data.date).toISOString().split('T')[0];
         // alert(`Vous avez selectionnéle ${selectedDate}`)
+        displayMovie(selectedDate)
     },
      months: [
         "Janvier",
@@ -33,4 +34,11 @@ function addMovie(e) {
     const duration = formData.get('duration');
     const genre = formData.getAll('genre');
     console.log('movie',{title,year,duration,genre,selectedDate});
+}
+function displayMovie(date) {
+    let movies = JSON.parse(localStorage.getItem('movie')) || [];
+    let movie = movies.find(m => {
+        return m.selectedDate === date;
+    });
+    console.log("movie",movie);
 }
